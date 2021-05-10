2bc262c4ee9aad9d2ad78a48aaedda063cf04e14
Victor Akio <victorakioz@gmail.com>
2021-05-10 00:39:52 -0300

    Add a modal with project details

--- src/components/Projects/Project.tsx
+++ src/components/Projects/Project.tsx
@@ -1,17 +1,41 @@
+import { useState } from "react";
+import { FaTimesCircle } from "react-icons/fa";
 import styles from "./project.module.scss";
 
 interface ProjectProps {
   image: string;
   name: string;
   link: string;
+  description: string;
 }
 
-export function Project({ image, name, link }: ProjectProps) {
+export function Project({ image, name, link, description }: ProjectProps) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
-    <div className={styles.projectWrapper}>
-      <a href={link}>
+    <>
+      <div
+        className={styles.projectWrapper}
+        onClick={() => setIsModalOpen(true)}
+      >
         <img src={image} alt={name} />
-      </a>
-    </div>
+      </div>
+      <div
+        className={`${styles.projectOverlay} ${isModalOpen && styles.active}`}
+      >
+        <div className={styles.projectDetails}>
+          <img src={image} alt={name} />
+          <div>
+            <FaTimesCircle onClick={() => setIsModalOpen(false)} />
+            <h2>{name}</h2>
+            <p>{description}</p>
+
+            <a href={link} target="blank" rel="noopener noreferer">
+              Visite o site
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -13,21 +13,25 @@ export function Projects() {
           name="Site do CPS"
           image="/images/cps.jpg"
           link="https://www.cps.sp.gov.br"
+          description="Portal institucional do Centro Paula Souza, desenvolvido em Wordpress utilizando o modo multisite, para abrigar todos os sites ligados a instituição de forma centralizada e padronizada. O tema é personalizado e construído do zero."
         />
         <Project
           name="Akioflix"
           image="/images/akioflix.jpg"
           link="https://akioflix.vercel.app/"
+          description="Projeto desenvolvido na Imersão React da Alura, é uma interface similar ao Netflix, para explorar a flexibilidade que o ReactJS possibilita na hora de construir aplicações Web"
         />
         <Project
           name="Spotfinder"
           image="/images/spotfinder.jpg"
           link="https://spotfinder.vercel.app/"
+          description="Aplicação em ReactJS que faz integração com a API do spotify, permitindo fazer login através de uma conta do spotify e fazer buscas por artistas, músicas e albuns."
         />
         <Project
           name="Github Explorer"
           image="/images/github-explorer.jpg"
           link="https://github-explorer-eta.vercel.app/"
+          description="Utilizando a api de usuários do Github essa aplicação faz a busca de usuários do github, listando seus repositórios."
         />
       </div>
     </section>
